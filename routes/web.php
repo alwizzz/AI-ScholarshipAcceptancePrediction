80b58004153e01771a704360b190e0b24acd127c
weday21 <107946702+weday21@users.noreply.github.com>
2022-06-21 23:50:33 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/learning/discrete_probability', [ItemController::class, 'discrete_p
 Route::get('/learning/continous_probability', [ItemController::class, 'continous_probability']);
 Route::get('/learning/prediction', [ItemController::class, 'prediction']);
 Route::get('/about', [ItemController::class, 'about']);
+
+// test
